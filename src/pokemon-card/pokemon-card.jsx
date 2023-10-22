418a8fed8ccea55f29c3c056b59954e37483409c
RubyVine01 <leraprokofevaiera1@gmail.com>
2023-10-22 22:38:50 +0300

    add info

--- src/pokemon-card/pokemon-card.jsx
+++ src/pokemon-card/pokemon-card.jsx
@@ -1,11 +1,20 @@
 import styles from "./pokemon-card.module.css";
 
-export default function PokemonCard({pokemonData}) {
+export default function PokemonCard({ pokemonData }) {
   return (
     <div className={styles.card}>
-        
-      <img src={pokemonData.sprites.other['official-artwork'].front_default} alt="Pokemon"  />
+      <img
+        src={pokemonData.sprites.other["official-artwork"].front_default}
+        alt="Pokemon"
+      />
+      <h2>{pokemonData.name}</h2>
+      <ul>
+        {pokemonData.stats.map((entry) => (
+          <li key={entry.stat.name}>
+            {entry.stat.name}: {entry.base_stat}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
