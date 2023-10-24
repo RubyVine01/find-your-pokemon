bfffe197527ad0198dfaba9bb4b42f4050af69af
RubyVine01 <leraprokofevaiera1@gmail.com>
2023-10-24 15:04:07 +0300

    add const

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,6 +2,7 @@ import PokemonCard from "../../pokemon-card/pokemon-card";
 import styles from "./main.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { request } from "../../units/api";
+import { RENAME_BTN, SET_POKEMON } from "../../services/reducers/pokemon-reducer";
 
 export default function Main() {
   const dispatch = useDispatch();
@@ -13,12 +14,12 @@ export default function Main() {
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
     request(url)
       .then((data) => {
-        dispatch({ type: "SET_POKEMON", data });
+        dispatch({ type: SET_POKEMON, data });
       })
       .catch(console.error);
 
     if (newPokemon === null) {
-      dispatch({ type: "RENAME_BTN", newBtnText: "try again" });
+      dispatch({ type: RENAME_BTN, newBtnText: "try again" });
     }
   };
 
--- src/services/reducers/pokemon-reducer.js
+++ src/services/reducers/pokemon-reducer.js
@@ -1,10 +1,14 @@
+export const SET_POKEMON = 'SET_POKEMON'
+export const RENAME_BTN = 'RENAME_BTN'
+
+
 const defaultState = { pokemon: null, btnText: "find pokemon" };
 
 export const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "SET_POKEMON":
+    case SET_POKEMON:
       return { ...state, pokemon: action.data };
-    case "RENAME_BTN":
+    case RENAME_BTN:
       return { ...state, btnText: action.newBtnText };
     default:
       return state;
